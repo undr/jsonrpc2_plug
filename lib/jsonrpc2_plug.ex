01fd900a1cad7761c747ae53502fe432b906a1f3
andrei.lepeshkin <andrei.lepeshkin@bgaming.com>
2022-01-16 13:20:55 +0300

    Add an ability to send an error from an outer app

--- lib/jsonrpc2_plug.ex
+++ lib/jsonrpc2_plug.ex
@@ -1,23 +1,58 @@
 defmodule JSONRPC2Plug do
+  @behaviour Plug
+  require Logger
+
+  @impl true
   def init(handler),
     do: handler
 
-  @doc false
+  @impl true
   def call(%{method: "POST", body_params: %Plug.Conn.Unfetched{}}, _handler),
     do: raise "Plug the JSONRPC2Plug after Plug.Parsers"
 
+  @impl true
   def call(%{method: "POST", body_params: body_params} = conn, handler) do
-    response = case handler.handle(body_params, conn) do
-      []   -> ""
-      nil  -> ""
-      resp -> Poison.encode!(resp)
-    end
+    Logger.debug("JSONRPC2 Request: #{inspect(body_params)}")
+
+    response = execute_request(handler, body_params, conn)
+
+    Logger.debug("JSONRPC2 Response: #{response}")
 
     conn
     |> Plug.Conn.put_resp_header("content-type", "application/json")
     |> Plug.Conn.resp(200, response)
   end
 
+  @impl true
   def call(conn, _),
     do: Plug.Conn.resp(conn, 404, "")
+
+  def send_error(conn, :error, %Plug.Parsers.ParseError{} = ex),
+    do: send_error_response(conn, :parse_error, Exception.message(ex))
+  def send_error(conn, kind, reason) do
+    details =
+      case kind do
+        :error -> Exception.message(reason)
+        any    -> inspect(any)
+      end
+
+    send_error_response(conn, :internal_error, details)
+  end
+
+  defp send_error_response(conn, code, details) do
+    conn
+    |> Plug.Conn.put_resp_content_type("application/json")
+    |> Plug.Conn.send_resp(500, error(code, details))
+  end
+
+  defp error(code, details),
+    do: JSONRPC2Plug.Error.new(nil, code, details) |> Poison.encode!()
+
+  defp execute_request(handler, body_params, conn) do
+    case handler.handle(body_params, conn) do
+      []   -> ""
+      nil  -> ""
+      resp -> Poison.encode!(resp)
+    end
+  end
 end
