9067527d3fae09d159200c16e28028baa101d75f
undr <undr@yandex.ru>
2022-02-26 14:59:15 +0300

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -39,7 +39,8 @@ defmodule Jsonrpc2Plug.MixProject do
       {:plug, "~> 1.12"},
       {:poison, "~> 4.0"},
       {:gettext, "~> 0.19.0"},
-      {:dialyxir, "~> 0.3", only: :dev}
+      {:dialyxir, "~> 0.3", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
